f14af7a6f871dbf472acd977d63eac9ae0527d8b
leefall <leefall2@naver.com>
2022-06-30 17:10:26 +0900

    Update SCNV_Calling.sh

--- Code/ASLOH_Pipeline/SCNV_Calling.sh
+++ Code/ASLOH_Pipeline/SCNV_Calling.sh
@@ -2,14 +2,13 @@
 
 
 
-CNVFACETS=#cnv_facets.R path
-NormalBam=#Normal Bam File
-TumorBam=#Tumor Bam File
-TMP=#Temporal Directory
-Reference=#ucsc hg19
-Output=#OutputName
-Targetbed=#Target bed
-dbsnp=#dbsnp_138.hg19.vcf.gz
+CNVFACETS= #cnv_facets.R path
+NormalBam= #Normal Bam File
+TumorBam= #Tumor Bam File
+TMP= #Temporal Directory
+Output= #OutputName
+Targetbed= #Target bed
+dbsnp= #dbsnp_138.hg19.vcf.gz
 
 
 
